85a2af2fca2bc90a00b560b7150e3efa66102322
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-05-10 02:15:20 +0800

    Add test for Countries component and update component render

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,37 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
+import Countries from "./components/Countries";
 
 describe("Basic app structure", () => {
   test("renders 'Country State City' text", () => {
-    const { getByText } = render(<App />);
-    const element = getByText(/Country State City/);
+    render(<App />);
+    const element = screen.getByText("Country State City");
     expect(element).toBeInTheDocument();
   });
 
   test("renders 'Countries' text", () => {
-    const { getByText } = render(<App />);
-    const element = getByText(/Countries/);
+    render(<App />);
+    const element = screen.getByText("Countries");
     expect(element).toBeInTheDocument();
   });
 
   test("renders 'Cities' text", () => {
-    const { getByText } = render(<App />);
-    const element = getByText(/Cities/);
+    render(<App />);
+    const element = screen.getByText("Cities");
     expect(element).toBeInTheDocument();
   });
 
   test("renders 'States' text", () => {
-    const { getByText } = render(<App />);
-    const element = getByText(/States/);
+    render(<App />);
+    const element = screen.getByText("States");
     expect(element).toBeInTheDocument();
+  });
+});
+
+describe("Countries structure", () => {
+  test("renders all 247 countries", async () => {
+    render(<Countries />);
+    const items = await screen.findAllByRole("list");
+    expect(items).toHaveLength(250);
   });
 });
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,7 +1,22 @@
 import React from "react";
+import countriesJSON from "../geoData/countries.json";
 
 function Countries() {
-  return <div className="countries">Countries</div>;
+  return (
+    <div className="countries">
+      <div>Countries</div>
+      <input type="text" placeholder="Search Countries..." />
+      <div className="countryList">
+        {countriesJSON.map((countries) => {
+          return (
+            <ul key={countries.id}>
+              <li>{countries.name}</li>
+            </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Countries;
