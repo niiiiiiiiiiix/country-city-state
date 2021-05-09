05c196048534e05fe48d0460610d2686d14071b9
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-05-10 00:15:06 +0800

    Create intial divs for basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <div>Hello World</div>
+      <div className="title"></div>
+      <div className="container">
+        <div className="country"></div>
+        <div className="states"></div>
+        <div className="cities"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
