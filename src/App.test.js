1a56b6fe3947d89303a2754a28209d3c21a4f8d0
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-05-10 02:51:12 +0800

    Add filter function alongside corresponding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 import Countries from "./components/Countries";
 
 describe("Basic app structure", () => {
-  test("renders 'Country State City' text", () => {
+  it("should render 'Country State City' text", () => {
     render(<App />);
     const element = screen.getByText("Country State City");
     expect(element).toBeInTheDocument();
   });
 
-  test("renders 'Countries' text", () => {
+  it("should render 'Countries' text", () => {
     render(<App />);
     const element = screen.getByText("Countries");
     expect(element).toBeInTheDocument();
   });
 
-  test("renders 'Cities' text", () => {
+  it("should render 'Cities' text", () => {
     render(<App />);
     const element = screen.getByText("Cities");
     expect(element).toBeInTheDocument();
   });
 
-  test("renders 'States' text", () => {
+  it("should render 'States' text", () => {
     render(<App />);
     const element = screen.getByText("States");
     expect(element).toBeInTheDocument();
@@ -29,9 +29,25 @@ describe("Basic app structure", () => {
 });
 
 describe("Countries structure", () => {
-  test("renders all 247 countries", async () => {
+  beforeEach(() => {
     render(<Countries />);
+  });
+
+  it("should render all 247 countries by default", async () => {
     const items = await screen.findAllByRole("list");
     expect(items).toHaveLength(250);
+  });
+
+  it("verify input is correct", () => {
+    const input = screen.getByLabelText("country-search");
+    fireEvent.change(input, { target: { value: "Singapore" } });
+    expect(input.value).toBe("Singapore");
+  });
+
+  it("should return only Singapore if user searches for Singapore", async () => {
+    const input = screen.getByLabelText("country-search");
+    fireEvent.change(input, { target: { value: "Singapore" } });
+    const items = await screen.findAllByRole("list");
+    expect(items).toHaveLength(1);
   });
 });
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,19 +1,35 @@
-import React from "react";
 import countriesJSON from "../geoData/countries.json";
+import { useState } from "react";
 
 function Countries() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="countries">
       <div>Countries</div>
-      <input type="text" placeholder="Search Countries..." />
+      <input
+        type="text"
+        placeholder="Search Countries..."
+        onChange={(event) => setSearchTerm(event.target.value)}
+        aria-label="country-search"
+      />
       <div className="countryList">
-        {countriesJSON.map((countries) => {
-          return (
-            <ul key={countries.id}>
-              <li>{countries.name}</li>
-            </ul>
-          );
-        })}
+        {countriesJSON
+          .filter((countries) => {
+            if (searchTerm === "") {
+              return countries;
+            } else if (
+              countries.name.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return countries;
+            }
+          })
+          .map((countries) => {
+            return (
+              <ul key={countries.id}>
+                <li>{countries.name}</li>
+              </ul>
+            );
+          })}
       </div>
     </div>
   );
