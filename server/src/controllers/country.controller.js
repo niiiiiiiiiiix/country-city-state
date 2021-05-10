a3f382de68b22486bcabac24eadbc3fb2be3bc9b
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-05-10 22:26:05 +0800

    Add route and test for getting specific country

--- server/src/controllers/country.controller.js
+++ server/src/controllers/country.controller.js
@@ -9,6 +9,16 @@ const getAllCountries = async (next) => {
   }
 };
 
+const getSpecificCountry = async (name, next) => {
+  try {
+    const countries = await Country.findOne({ name: name });
+    return countries;
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllCountries,
+  getSpecificCountry,
 };
--- server/src/routes/country.route.js
+++ server/src/routes/country.route.js
@@ -4,8 +4,16 @@ const ctrl = require("../controllers/country.controller");
 
 country.get("/", async (req, res, next) => {
   try {
-    const countries = await ctrl.getAllCountries();
-    console.log(countries);
+    const countries = await ctrl.getAllCountries(next);
+    res.status(200).json(countries);
+  } catch (error) {
+    next(error);
+  }
+});
+
+country.get("/:name", async (req, res, next) => {
+  try {
+    const countries = await ctrl.getSpecificCountry(req.params.name, next);
     res.status(200).json(countries);
   } catch (error) {
     next(error);
